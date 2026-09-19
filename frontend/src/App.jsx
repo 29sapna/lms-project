3464a4d64672e6e9b5e82de9c4de176084c37545
Sapna Tiwari <sapnatiwari9369516@gmail.com>
2026-09-19 17:41:54 +0530

    Add lms

---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Courses from "./components/pages/Courses";
+import Detail from "./components/pages/Detail";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import MyCourses from "./components/pages/account/MyCourses";
+import MyLearning from "./components/pages/MyLearning";
+import WatchCourse from "./components/pages/account/WatchCourse";
+import ChangePassword from "./components/pages/account/ChangePassword";
+import { Toaster } from "react-hot-toast";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/detail" element={<Detail />} />
+          <Route path="/account/login" element={<Login />} />
+          <Route path="/account/register" element={<Register />} />
+          <Route path="/account/my-courses" element={<MyCourses />} />
+          <Route path="/account/courses-enrolled" element={<MyLearning />} />
+          <Route path="/account/watch-course" element={<WatchCourse />} />
+          <Route path="/account/change-password" element={<ChangePassword />} />
+        </Routes>
+      </BrowserRouter>
+      <Toaster position="top-center" reverseOrder={false} />
+    </>
+  );
+}
+
+export default App;
